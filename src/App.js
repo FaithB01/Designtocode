8875529b78ec4db87cc90b603baca53920075708
Faith <fnbiketi@gmail.com>
2021-07-01 14:39:12 -0700

    List Rendering

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Profile from './components/Profile';
-import Projects from './components/Projects';
-import Activity from './components/Activity';
+import ActivityItemList from './components/ActivityItemList';
+import ProjectList from './components/ProjectList';
 
 
 
@@ -13,8 +13,8 @@ function App() {
       <Header />
       <section className="row">
         <Profile name="Debbie Lewis" username="debbielewis" avatar="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&h=256&q=80" projectCount="8" membership="Pro Member" githubUrl="https://example.com/debbielewis" />
-        <Projects />
-        <Activity />
+        <ProjectList />
+        <ActivityItemList />
       </section>
     </div>
   );
--- src/components/ActivitItems.js
+++ src/components/ActivitItems.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
 
-const ActivitItems = ({ project, commit, environment, time }) => {
+
+function ActivitItems({ item }) {
+
   return (
     <>
       <ul className="aci ">
-        <li className="activity-item ">
+        <li className="activity-item">
           <div className="combine ">
             <div className="together ">
               <h3 className="t-name "><img src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&h=256&q=80 " width="30 " height="30 " alt=" " />
                 <div className="omg">You</div>
               </h3>
-              <p className="time ">{time}</p>
+              <p className="time ">{item.time}</p>
             </div>
-            <p className="statement ">Deployed {project} ({commit} in master) to {environment}</p>
+            <p className="statement ">Deployed {item.project} ({item.commit} in master) to {item.environment}</p>
           </div>
         </li>
       </ul>
